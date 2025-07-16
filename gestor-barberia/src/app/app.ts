ec620bc2e9552b513c810f190aa1189680282934
AgenSmith04 <kevnivesela@gmail.com>
2025-07-15 23:32:34 -0500

    base

--- gestor-barberia/src/app/app.ts
+++ gestor-barberia/src/app/app.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { environment } from '../environments/environment';
+
 
 @Component({
   selector: 'app-root',
--- gestor-barberia/src/main.ts
+++ gestor-barberia/src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { environment } from './environments/environment';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    // otros providers...
+  ]
+});
